6c89015c687a646a4fbb310253c8fac6fe37d97b
kameni <kameni.nt@gmail.com>
2025-10-28 10:38:42 -0700

    Add asset dependency metadata for weight loss block

---
+++ generatepress-child/blocks/weight-loss-calculator/editor.asset.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'dependencies' => [
+        'wp-blocks',
+        'wp-element',
+        'wp-components',
+        'wp-block-editor',
+        'wp-i18n',
+        'wp-data'
+    ],
+    'version' => filemtime(__DIR__ . '/editor.js'),
+];
